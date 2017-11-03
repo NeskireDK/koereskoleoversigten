9c5a13975539a9ff6ac252ffab3b7afb16e801d9
Benjamin <benjaco1996@gmail.com>
2017-11-03 15:05:49 +0100

    comments

--- server/index.js
+++ server/index.js
@@ -1,22 +1,22 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 
+const app = express();
+
 // serve index file from static assets
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/../static/index.html'));
 });
 
 // handle all api requests
-// this is just an example
-app.get("/api/test", function (reg, res) {
-    res.json({ content: "test" });
+app.get("/api/example", function (reg, res) { // this is just an example
+    res.json({content: "example"});
 });
 
 // handle unmatched api requests before serving static assets
 app.all("/api/*", function (reg, res) {
     res.status(404);
-    res.json({ status: "error"})
+    res.json({status: "error"})
 });
 
 // any thing else is found in the static folder
@@ -24,3 +24,5 @@ app.use('/', express.static(__dirname + '/../static'));
 
 // start the server
 app.listen(80);
+
+
