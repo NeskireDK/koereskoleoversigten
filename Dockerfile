255e6ce4bf8e9bfed03ef2234bebf8138688e915
Benjamin <benjaco1996@gmail.com>
2017-11-03 14:52:00 +0100

    comments

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,13 @@ FROM node:carbon
 WORKDIR /usr/src/app
 
 # Install app dependencies
-COPY package.json .
-# For npm@5 or later, copy package-lock.json as well
-# COPY package.json package-lock.json ./
-
+COPY package.json package-lock.json ./
 RUN npm install
 
+# Copy code
 COPY server server/
 COPY static static/
 
-
+# Start container
 EXPOSE 80
 ENTRYPOINT [ "npm", "start" ]
--- server/index.js
+++ server/index.js
@@ -1,24 +1,26 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 
+// serve index file from static assets
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../static/index.html'));
 });
 
+// handle all api requests
+// this is just an example
 app.get("/api/test", function (reg, res) {
-    res.send("test");
+    res.json({ content: "test" });
 });
 
-
-app.get("/api/*", function (reg, res) {
-    res.send("Endpoint not found");
-});
-app.post("/api/*", function (reg, res) {
-    res.send("Endpoint not found");
+// handle unmatched api requests before serving static assets
+app.all("/api/*", function (reg, res) {
+    res.status(404);
+    res.json({ status: "error"})
 });
 
-
+// any thing else is found in the static folder
 app.use('/', express.static(__dirname + '/../static'));
 
+// start the server
 app.listen(80);
