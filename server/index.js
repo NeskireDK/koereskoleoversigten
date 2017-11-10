577074dec5262223d7d37421f0ed8572c6bc333e
Benjamin <benjaco1996@gmail.com>
2017-11-10 14:26:52 +0100

    variable refactoring

--- server/index.js
+++ server/index.js
@@ -2,43 +2,40 @@ const express = require('express');
 const path = require('path');
 const database = require("./database");
 
-
-const app = express();
+const route = express();
 
 // serve index file from static assets
-app.get('/', function (req, res) {
+route.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/../static/index.html'));
 });
 
-app.get("/api/test", async (req, res) => {
-    const [data, metadata] = await database.execute("SELECT * FROM `test` where id = ?", [1]);
-
+route.get("/api/test", async (req, res) => {
+    let [data, metadata] = await database.execute("SELECT * FROM `test` where id = ?", [1]);
     res.json({
         data,
         metadata
     })
-
 });
 
 // handle all api requests
-app.get("/api/example", function (req, res) { // this is just an example
+route.get("/api/example", (req, res) => { // this is just an example
     res.json({content: "example"});
 });
 
 // handle unmatched api requests before serving static assets
-app.all("/api/*", function (req, res) {
+route.all("/api/*", (req, res) => {
     res.status(404);
     res.json({status: "error"})
 });
 
 // anything else is found in the static folder
-app.use('/', express.static(__dirname + '/../static'));
+route.use('/', express.static(__dirname + '/../static'));
 
 
 // start the server
 (async ()=>{
     await database.createPool();
-    app.listen(80);
+    route.listen(80);
 })();
 
 
