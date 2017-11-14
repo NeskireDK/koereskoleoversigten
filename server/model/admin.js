6aaef2d033d85c27f69b530c48efe39a00a03920
André Eriksen <neskire@outlook.com>
2017-11-14 22:43:59 +0100

    Admin: insert, get, getall, Try/catch

--- server/model/admin.js
+++ server/model/admin.js
@@ -1,4 +1,34 @@
-const database = require("./database");
-function GetUsers(){
-    let [data, metadata] = await database.execute("SELECT * FROM `admin`");
+import * as database from "../database"
+
+export default class Admin {
+
+
+    constructor(name, email, password, ) {
+        this.name = name
+        this.email = email
+        this.password = password
+    }
+
+    static async getAll(){
+        let [data, metadata] = await database.execute("SELECT * FROM `admin`")
+        return [data, metadata]
+    }
+    static async get(id){
+        let [data, metadata] = await database.execute("SELECT * FROM `admin` where id = ?", [id])
+        return data
+    }
+    static async insert(name, email, password){
+        try {
+            let [data, metadata] = await database.execute(`
+            INSERT INTO admin 
+            SET name=?, email=?, password=?`,
+                [name, email, password])
+            return new Admin(name, email, password);
+        }catch(err) {
+            return {error : err}
+        }
+
+    }
+
 }
+
