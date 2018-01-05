bba837b523b4a64f53a48a5c97041c5843bc6192
André Eriksen <neskire@outlook.com>
2018-01-05 15:33:21 +0100

    #9, #10
    Basic validation og mere course

--- server/model/course.mjs
+++ server/model/course.mjs
@@ -1,9 +1,36 @@
-import * as database from "../database"
-
+import * as database from '../database'
+import moment from 'moment'
 export default class Course {
+    // Generic Contructor to accept all properties
+    constructor(data){
+        for(let prop of Object.keys(data)){
+            this[prop]=data[prop]
+        }
+    }
+
+    
     static async get(id){
         let [data, metadata] = await database.execute("SELECT * FROM `course` where id = ?", [id])
         return data
     }
-
+    static async getAll(id){
+        let [data, metadata] = await database.execute("SELECT * FROM `course`")
+        return data
+    }
+    async insert(){
+        try {
+            
+            moment.locale('da')
+            this.created = moment().format("YYYY-MM-DD HH:mm:ss")
+            let [data, metadata] = await database.query(`
+            INSERT INTO course 
+            SET ?`,
+                this)
+            console.info("Course "+this.title+" Created for school: " + this.school_id)
+            return this;
+        }catch(err) {
+            console.warn("Failed to create Course "+this.title+" Created for school: " + this.school_id)
+            return {error : err}
+        }
+    }
 }
--- server/routes/courseRoute.mjs
+++ server/routes/courseRoute.mjs
@@ -1,9 +1,28 @@
 import course from '../model/course.mjs';
-
+import validate from 'validate.js'
 export default class courseRoute {
     static Initialize(route) {
+        validate.validators.presence.options = {message: "må ikke være tomt"};
+        validate.validators.length.options = {tooShort: "skal være mindst %{count} karakterer langt"};
+        var courseConstraints = {
+            school_id: {
+              presence: true,
+              numericality: {
+                onlyInteger: true,
+                greaterThan: 0,
+              },
+              
+            },
+            title: {
+              presence: true,
+              length: {
+                minimum: 6,
+              }
+            }
+          };          
 
-        // Get All courses
+        
+          // Get All courses
         route.get("/api/course", async (req, res) => {
             let a = await course.getAll()
             res.json({
@@ -22,10 +41,17 @@ export default class courseRoute {
         });
         // Insert course via Post
         route.post("/api/course/", async (req, res) => {
-            const obj = new course({ name: req.body.name, email: req.body.email, password: req.body.password })
-            res.json({
-                res: await obj.insert()
-            })
+            // validate all properties
+            let val;
+            if(val = validate({school_id: req.body.school_id, title: req.body.title}, courseConstraints)){
+                console.log("Validation: "+JSON.stringify(val))
+                res.status(400).json(val)
+            }else {
+                const obj = new course({ title: req.body.title, description: req.body.description, school_id: req.body.school_id })
+                res.json({
+                    res: await obj.insert()
+                }) 
+            }
         });
         // Update course via Post
         route.put("/api/course/:id", async (req, res) => {
