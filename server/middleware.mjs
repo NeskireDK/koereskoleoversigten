5a4fd27d0dad734d6bb00873afcdb32940458fdc
Benjamin <benjaco1996@gmail.com>
2018-01-10 14:09:45 +0100

    formatting

--- server/middleware.mjs
+++ server/middleware.mjs
@@ -4,12 +4,12 @@ import v from "./validate";
 v.setupValidationMessages(validate);
 
 
-export const validatePost = function(validationConstraints) {
-    return function(req, res, next) {
+export const validatePost = function (validationConstraints) {
+    return function (req, res, next) {
         let validationError = validate(req.body, validationConstraints);
-        if(typeof validationError === "undefined") {
+        if (typeof validationError === "undefined") {
             next()
-        }else{
+        } else {
             res.status(400).json(validationError)
         }
     }
