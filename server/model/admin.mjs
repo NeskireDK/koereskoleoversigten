38ddc692ad271565d32b9df084b9d7a24c1f7cce
André Eriksen <neskire@outlook.com>
2018-01-12 16:25:20 +0100

    Course api and admin api #12 close

--- server/model/admin.mjs
+++ server/model/admin.mjs
@@ -2,49 +2,54 @@ import * as database from "../database"
 import admin from "../routes/adminRoute.mjs";
 
 export default class Admin {
-    constructor(data){
-        for(let prop of Object.keys(data)){
-            this[prop]=data[prop]
+    constructor(data) {
+        for (let prop of Object.keys(data)) {
+            this[prop] = data[prop]
         }
     }
-    static async getAll(){
+
+    static async getAll() {
         let [data, metadata] = await database.execute("SELECT * FROM `admin`")
         console.log("a console.log message")
         return [data, metadata]
     }
-    static async get(id){
+
+    static async get(id) {
         let [data, metadata] = await database.execute("SELECT * FROM `admin` where id = ?", [id])
         return new Admin(data[0])
     }
-    async insert(){
+
+    async insert() {
         try {
             let [data, metadata] = await database.query(`
             INSERT INTO admin 
             SET ?`,
                 this)
             return this;
-        }catch(err) {
-            return {error : err}
+        } catch (err) {
+            return {error: err}
         }
     }
-    async update(){
-        console.log('Doing stuff')
+
+    async update() {
+        console.log('Running update')
         try {
             let [data, metadata] = await database.query(`
             UPDATE admin 
             SET name=?,
-            email=?,
+            email   =?,
             password=?,
-            media_id=?,            
-            WHERE id=?`,
+            media_id=?            
+            WHERE id=?`, [
                 this.name,
                 this.email,
                 this.password,
                 this.media_id,
-                this.id)
+                this.id
+            ])
             return data;
-        }catch(err) {
-            return {error : err}
+        } catch (err) {
+            return {error: err}
         }
     }
 }
--- server/model/course.mjs
+++ server/model/course.mjs
@@ -1,57 +1,67 @@
 import * as database from '../database'
 import moment from 'moment'
+
 export default class Course {
     // Generic Contructor to accept all properties
-    constructor(data){
-        for(let prop of Object.keys(data)){
-            this[prop]=data[prop]
+    constructor(data) {
+        for (let prop of Object.keys(data)) {
+            this[prop] = data[prop]
         }
     }
-    
-    static async get(id){
-        let [data, metadata] = await database.execute("SELECT * FROM `course` where id = ?", [id])
-        if(!data[0]){
-            console.info("No such course by id: "+id)
+
+    static async get(id) {
+        let [data, metadata] = await database.execute("SELECT * FROM `course` WHERE ID = ?", [id])
+        if (!data[0]) {
+            console.info("No such course by id: " + id)
             return null
         }
         return new Course(data[0])
     }
-    static async getAll(id){
-        let [data, metadata] = await database.execute("SELECT * FROM `course`")
+
+    static async getAll(id) {
+        let [data, metadata] = await database.execute("SELECT * FROM course")
         return data
     }
-    async insert(){
+
+    async insert() {
         try {
             moment.locale('da')
             this.created = moment().format("YYYY-MM-DD HH:mm:ss")
             let [data, metadata] = await database.query(`
-            INSERT INTO course 
-            SET ?`,
+                  INSERT INTO course
+                  SET ?`,
                 this)
-            console.info("Course "+this.title+" Created for school: " + this.school_id)
+            console.info("Course " + this.title + " Created for school: " + this.school_id)
             return this;
-        }catch(err) {
-            console.warn("Failed to create Course "+this.title+" Created for school: " + this.school_id)
-            return {error : err}
+        } catch (err) {
+            console.warn("Failed to create Course " + this.title + " Created for school: " + this.school_id)
+            return {error: err}
         }
     }
-    async update(){
-        // try {
+
+    async update() {
+        try {
             moment.locale('da')
             this.updated = moment().format("YYYY-MM-DD HH:mm:ss")
             let [data, metadata] = await database.query(`
-            UPDATE course 
-            SET ? 
-            WHERE id = ?`,
-                this, this.id)
-            console.info("Course "+this.title+" updated for school: " + this.school_id)
+                UPDATE course
+                    SET title   = ?,
+                    description = ?,
+                    updated     = ?
+                WHERE id = ?`, [
+                this.title,
+                this.description,
+                this.updated,
+                this.id
+            ])
+            console.info("Course " + this.title + " updated for school: " + this.school_id)
             return this;
-        // }catch(err) {
-        //     console.warn("Failed to Update Course "+this.title+" for school_Id: " + this.school_id)
-        //     for(let key of Object.keys(err)){
-        //         console.log(key +' = '+ err[key])
-        //     }
-        //     return {error : err}
-        // }
+        } catch (err) {
+            console.warn("Failed to Update Course " + this.title + " for school_Id: " + this.school_id)
+            for (let key of Object.keys(err)) {
+                console.warn(key + ' = ' + err[key])
+            }
+            return {error: err.toJSON()}
+        }
     }
 }
